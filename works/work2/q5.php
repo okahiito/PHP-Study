a290110b3e9313d3ff55b27d07b6f4970d337631
302A-01\cre <a>
2025-08-26 12:16:34 +0900

    a

--- works/work2/q5.php
+++ works/work2/q5.php
@@ -1,3 +1,8 @@
 <?php
 // 配列の全ての要素を2倍にして表示するコードを記述してください(for文を使うこと)
 $numbers = array(1, 2, 3, 4, 5);
+
+for ($i = 0; $i < count($numbers); $i++) {
+    $numbers[$i] = $numbers[$i] * 2;
+    echo $numbers[$i] . "\n";
+}
